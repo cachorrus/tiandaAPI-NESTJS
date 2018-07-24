94fa6e30cb37e30535e4fb41491d93e16cefb7c1
DESKTOP-FIPOO7U\cachorro <mario.martinez.zavala@gmail.com>
2018-07-24 13:06:02 -0500

    update npm version move public folder and update path

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import * as cors from 'cors';
 import { AppModule } from './app.module';
 
 // import * as express from 'express';
-// import { join } from 'path';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // app.use(express.static(join(__dirname, 'public')));
-  app.useStaticAssets(__dirname + '/public'); // http://localhost:3000/img/productos/S10_1949.jpg
+  // app.useStaticAssets(__dirname + '/public'); // http://localhost:3000/img/productos/S10_1949.jpg
+  app.useStaticAssets(join(__dirname + './../public'));
 
   // const allowedOrigins = [
   //                     'http://localhost:3000',
