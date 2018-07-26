fe797988b566b2568fc0a64e97fb2af220e563f6
DESKTOP-FIPOO7U\cachorro <mario.martinez.zavala@gmail.com>
2018-07-26 17:01:06 -0500

    install dotenv configurar nodemon.js para preload del archivo .env se agrego ejemplo del archivo .env.example

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
 
   // app.use(express.static(join(__dirname, 'public')));
